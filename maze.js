5e65ff68d532c3c1351e4ba799d2b3f395292f1c
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-07-27 15:07:32 -0700

    randomized start and end points

--- maze.js
+++ maze.js
@@ -31,7 +31,7 @@ function createWalls()
     var item = $("<div class='col'></div>");
     for (var j = 0; j < height; j++)
     {
-      item.append("<div class='wall' id='"+i+""+j+"'></div>")
+      item.append("<div class='wall' id='x"+i+"y"+j+"'></div>")
     }
     $("#container").append(item);
     $("body").append($("#container"));
@@ -48,11 +48,62 @@ function createArray()
     }
     array.push(tempArray);
   }
-  console.log(array);
+  // console.log(array);
 }
 function test()
 {
-  var one = Math.floor(Math.random()*50);
-  var two = Math.floor(Math.random()*height);
-  $("#"+one+two).css("background-color", "white");
+  var start;
+  var end;
+
+  var side = Math.floor(Math.random()*4);
+  if (side === 0)
+  {
+    var one = "x"+Math.floor(Math.random()*50);
+    var two = "y"+0;
+    start = one+""+two;
+
+    var one = "x"+Math.floor(Math.random()*50);
+    var two = "y"+(height-1);
+    end = one+""+two;
+  }
+  else if (side === 1)
+  {
+    var one = "x"+49;
+    var two = "y"+Math.floor(Math.random()*height);
+    start = one+""+two;
+
+    var one = "x"+0
+    var two = "y"+Math.floor(Math.random()*height);
+    end = one+""+two;
+  }
+  else if (side === 2)
+  {
+    var one = "x"+Math.floor(Math.random()*50);
+    var two = "y"+(height-1);
+    start = one+""+two;
+
+    var one = "x"+Math.floor(Math.random()*50);
+    var two = "y"+0;
+    end = one+""+two;
+  }
+  else if (side === 3)
+  {
+    var one = "x"+0
+    var two = "y"+Math.floor(Math.random()*height);
+    start = one+""+two;
+
+    var one = "x"+49;
+    var two = "y"+Math.floor(Math.random()*height);
+    end = one+""+two;
+  }
+
+  // var one = Math.floor(Math.random()*50);
+  // var two = Math.floor(Math.random()*height);
+  // console.log(one+""+two);
+  console.log("side: "+side);
+  console.log("start: "+start);
+  console.log("end: "+end);
+  // $("#"+one+two).css("background-color", "white");
+  $("#"+start).css("background-color", "green");
+  $("#"+end).css("background-color", "red");
 }
