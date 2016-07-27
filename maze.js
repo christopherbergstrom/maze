e5430311eb59bbf3a170aba8bb73b024c29b3c31
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-07-27 13:52:52 -0700

    divs made

--- maze.js
+++ maze.js
@@ -1,4 +1,20 @@
 $(document).ready(function()
 {
-  console.log("loaded");
+  createWalls();
 });
+function createWalls()
+{
+  $("body").append("<div id='container'></div>");
+  var width = window.innerWidth*.02;
+  var height = Math.floor(window.innerHeight / width);
+  for (var i = 0; i < height; i++)
+  {
+    var item = $("<div class='row'></div>");
+    for (var j = 0; j < 50; j++)
+    {
+      item.append("<div class='wall'></div>")
+    }
+    $("#container").append(item);
+    $("body").append($("#container"));
+  }
+}
