55cc950e108eea38c070d48fbb266c84bfa5ffdd
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-07-29 19:47:35 -0700

    fixed little scroll bug

--- maze.js
+++ maze.js
@@ -84,7 +84,8 @@ function createDivs()
   // create divs for maze map
   $("body").append("<div id='container'></div>");
   var width = window.innerWidth*difficulty;
-  height = Math.floor(window.innerHeight / width);
+  height = Math.floor((window.innerHeight) / (width-1));
+  // height-=1;
   for (var i = 0; i < maxWidth; i++)
   {
     if (difficulty === .04)
